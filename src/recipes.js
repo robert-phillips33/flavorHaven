f5337ac795e9fcd474e37be0f64c5f940ffb7cf5
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-07-07 14:57:27 -0400

    Updated some functionality to recipes file.

--- src/recipes.js
+++ src/recipes.js
@@ -1,6 +1,36 @@
 //Here is an example demonstrating logic separated that can be imported into the scripts and test files. Feel free to update this later! 
 
+// import ingredientsData from './data/ingredientsData.js';
+import recipeData from './data/recipes.js';
+
 export const findRecipeIngredients = recipe => {
   console.log(recipe)
 }
 
+// Return a filtered list of recipes based on a tag. (Extension option: filtering by multiple tags)
+export const findRecipeTag = (recipe, tag) => {
+  let thatRecipe = recipe.filter((recipes) => {
+    return recipes.tags.includes(tag)
+  });
+  let mapRecipe = thatRecipe.map((food) => {
+    return { id: food.name, tags: food.tags }
+    // return food.name
+  });
+  return mapRecipe
+};
+
+// console.log(findRecipeTag(recipeData, 'sauce'))
+
+// Return a filtered list of recipes based on a recipe name. (Extension option: filtering by name or ingredients)
+
+export const findRecipeName = (recipe, name) => {
+  let thisRecipe = recipe.filter((recipes) => {
+    if (recipes.name.includes(name)) {
+      return recipe
+    }
+  });
+  return thisRecipe
+};
+
+// console.log(findRecipeName(recipeData, 'Bang Bang Shrimp with Napa Cabbage Slaw'))
+// Determine the names of ingredients needed for a given recipe.
--- test/recipes-test.js
+++ test/recipes-test.js
@@ -1,8 +1,26 @@
 import { expect } from 'chai';
-import { findRecipeIngredients } from '../src/recipes';
+import { findRecipeIngredients, findRecipeTag, findRecipeName } from '../src/recipes';
+import recipeData from '../src/data/recipes';
 
-describe('Recipe', () => {
+describe('findRecipeIngredients', () => {
   it('Should be a function', () => {
     expect(findRecipeIngredients).to.be.a('function');
   });
 })
+
+describe('findRecipeTag', () => {
+  it.skip('Should be a function', () => {
+    expect(findRecipeTag).to.be.a('function');
+  });
+
+  it.skip('Should return a filtered list of recipes based on a tag', () => {
+    const recipeTag = findRecipeTag(recipeData, 'sauce')
+    expect(recipeTag).to.deep.equal('Dirty Steve\'s Original Wing Sauce')
+  });
+});
+
+describe('findRecipeName', () => {
+  it.skip('Should be a function', () => {
+    expect(findRecipeName).to.be.a('function');
+  });
+});
