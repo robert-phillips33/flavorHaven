6a5b92bf225e1874d7370d953970b16037ff1d8b
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-07-13 19:02:08 -0400

    Forgot to change data name

--- test/recipes-test.js
+++ test/recipes-test.js
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 // const expect = chai.expect;
 import { findRecipeIngredients, findRecipeTag, findRecipeName, findRecipePrice, findRecipeInstructions } from '../src/recipes.js';
 // import { ingredientsPromise, ingredientList, handleIngredients, handleRecipes,  recipesPromise } from '../src/apiCalls.js';
-import recipeData from '../src/data/recipes.js';
+// import recipeData from '../src/data/recipes.js';
 import ingredientsData from '../src/data/ingredients.js';
 import newRecipeData from '../src/data/recipesSample.js';
 
@@ -36,7 +36,7 @@ describe('findRecipeTag', () => {
   });
 
   it('Should return a filtered list of recipes based on a tag', () => {
-    const recipeTag = findRecipeTag(recipeData, 'sauce')
+    const recipeTag = findRecipeTag(newRecipeData, 'sauce')
     expect(recipeTag).to.deep.equal(['Dirty Steve\'s Original Wing Sauce'])
   });
 });
@@ -47,7 +47,7 @@ describe('findRecipeName', () => {
     expect(findRecipeName).to.be.a('function');
   });
   it('should get a matching name', () => {
-    const recipeName = findRecipeName(recipeData, 'Chocolate');
+    const recipeName = findRecipeName(newRecipeData, 'Chocolate');
     expect(recipeName[0].name).to.equal('Loaded Chocolate Chip Pudding Cookie Cups')
   });
 });
@@ -58,7 +58,7 @@ describe('findRecipePrice', () => {
   });
 
   it('should have the right price total', () => {
-    const recipePrice = findRecipePrice(recipeData[5])
+    const recipePrice = findRecipePrice(newRecipeData[5])
     expect(recipePrice).to.equal(18880.25);
   });
 });
@@ -69,24 +69,36 @@ describe('findRecipeInstructions', function() {
   });
 
   it('should return the instructions for a given recipe', function() {
-    const userRecipe = findRecipeInstructions(recipeData, 'Pastry Cream')
+    const userRecipe = findRecipeInstructions(newRecipeData, 'Elvis Pancakes')
     expect(userRecipe).to.deep.equal([
       {
-        instruction: 'In a heavy saucepan, stir together the milk and 1/4 cup of sugar. Bring to a boil over medium heat.',
-        number: 1
-      },
-      {
-        instruction: "In a medium bowl, whisk together the egg yolks and egg. Stir together the remaining sugar and cornstarch; then stir them into the egg until smooth. When the milk comes to a boil, drizzle it into the bowl in a thin stream while mixing so that you do not cook the eggs. Return the mixture to the saucepan, and slowly bring to a boil, stirring constantly so the eggs don' t curdle or scorch on the bottom.",
-        number: 2
-      },
-      {
-        instruction: 'When the mixture comes to a boil and thickens, remove from the heat. Stir in the butter and vanilla, mixing until the butter is completely blended in.',
-        number: 3
-      },
-      {
-        instruction: 'Pour into a heat-proof container and place a piece of plastic wrap directly on the surface to prevent a skin from forming. Refrigerate until chilled before using.',
-        number: 4
-      }
+        "instruction": "Watch how to make this recipe.",
+        "number": 1
+    },
+    {
+        "instruction": "In a large bowl, whisk together buttermilk, eggs, baking powder, sugar, salt and butter.",
+        "number": 2
+    },
+    {
+        "instruction": "In another large bowl mix together all-purpose flour and buckwheat flour.",
+        "number": 3
+    },
+    {
+        "instruction": "Slowly add flour into the wet ingredients mixing with a whisk.",
+        "number": 4
+    },
+    {
+        "instruction": "Mix until there are no lumps and the batter is smooth and velvety.",
+        "number": 5
+    },
+    {
+        "instruction": "In a large cast iron skillet or flat grill pan over medium-high heat, melt a tablespoon of butter. Ladle pancake batter onto skillet to desired size. Using the ladle, form pancake into circular shape. Cook on each side for 2 to 3 minutes or until golden brown. Set pancakes aside and keep warm. Repeat with remaining ingredients.",
+        "number": 6
+    },
+    {
+        "instruction": "Once completed, spread peanut butter on a pancake, layer it with sliced bananas and drizzle it with honey. Top the pancake with another pancake to form a sandwich. Repeat with remaining pancakes and serve with extra honey.",
+        "number": 7
+    }
     ])
   });
 });
