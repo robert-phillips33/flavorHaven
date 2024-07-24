a3280a09c6d2cae037f5616898074c25feed9dd9
ROlearyPro <rolearyprogramming@gmail.com>
2024-07-23 18:29:11 -0600

    revert

--- src/data/users.js
+++ src/data/users.js
@@ -1,249 +1,214 @@
-let userData = [
-  {
-    "id": 1,
-    "name": "Sadye Welch",
-    "recipesToCook": []
-  },
-  {
-    "id": 2,
-    "name": "Jordyn West",
-    "recipesToCook": []
-  },
-  {
-    "id": 3,
-    "name": "Willie Eichmann",
-    "recipesToCook": []
-  },
-  {
-    "id": 4,
-    "name": "Blanca Bergnaum",
-    "recipesToCook": []
-  },
-  {
-    "id": 5,
-    "name": "Wilhelm Lynch",
-    "recipesToCook": []
-  },
-  {
-    "id": 6,
-    "name": "Vincent Jacobs",
-    "recipesToCook": []
-  },
-  {
-    "id": 7,
-    "name": "Ellis Champlin",
-    "recipesToCook": []
-  },
-  {
-    "id": 8,
-    "name": "Velda Cassin",
-    "recipesToCook": []
-  },
-  {
-    "id": 9,
-    "name": "Maverick Hills",
-    "recipesToCook": []
-  },
-  {
-    "id": 10,
-    "name": "Salvatore Waters",
-    "recipesToCook": []
-  },
-  {
-    "id": 11,
-    "name": "Leda Johnson",
-    "recipesToCook": []
-  },
-  {
-    "id": 12,
-    "name": "Loma Konopelski",
-    "recipesToCook": []
-  },
-  {
-    "id": 13,
-    "name": "Karlee Upton",
-    "recipesToCook": []
-  },
-  {
-    "id": 14,
-    "name": "Domingo Auer",
-    "recipesToCook": []
-  },
-  {
-    "id": 15,
-    "name": "Carroll Maggio",
-    "recipesToCook": []
-  },
-  {
-    "id": 16,
-    "name": "Brock Lang",
-    "recipesToCook": []
-  },
-  {
-    "id": 17,
-    "name": "Kenyatta Trantow",
-    "recipesToCook": []
-  },
-  {
-    "id": 18,
-    "name": "Elbert Torphy",
-    "recipesToCook": []
-  },
-  {
-    "id": 19,
-    "name": "Muriel Muller",
-    "recipesToCook": []
-  },
-  {
-    "id": 20,
-    "name": "Cordell Reilly",
-    "recipesToCook": []
-  },
-  {
-    "id": 21,
-    "name": "Rachelle Borer",
-    "recipesToCook": []
-  },
-  {
-    "id": 22,
-    "name": "Demario Labadie",
-    "recipesToCook": []
-  },
-  {
-    "id": 23,
-    "name": "Bridie Hauck",
-    "recipesToCook": []
-  },
-  {
-    "id": 24,
-    "name": "Kylie Schroeder",
-    "recipesToCook": []
-  },
-  {
-    "id": 25,
-    "name": "Kobe Bauch",
-    "recipesToCook": []
-  },
-  {
-    "id": 26,
-    "name": "Adriana Roberts",
-    "recipesToCook": []
-  },
-  {
-    "id": 27,
-    "name": "Murl Bradtke",
-    "recipesToCook": []
-  },
-  {
-    "id": 28,
-    "name": "Dallin Hodkiewicz",
-    "recipesToCook": []
-  },
-  {
-    "id": 29,
-    "name": "Demetris Stokes",
-    "recipesToCook": []
-  },
-  {
-    "id": 30,
-    "name": "Felipe Bode",
-    "recipesToCook": []
-  },
-  {
-    "id": 31,
-    "name": "Reid Effertz",
-    "recipesToCook": []
-  },
-  {
-    "id": 32,
-    "name": "Caesar Harvey",
-    "recipesToCook": []
-  },
-  {
-    "id": 33,
-    "name": "Alford Brekke",
-    "recipesToCook": []
-  },
-  {
-    "id": 34,
-    "name": "Zane Sawayn",
-    "recipesToCook": []
-  },
-  {
-    "id": 35,
-    "name": "Jorge Bins",
-    "recipesToCook": []
-  },
-  {
-    "id": 36,
-    "name": "Serena Orn",
-    "recipesToCook": []
-  },
-  {
-    "id": 37,
-    "name": "Pansy Torp",
-    "recipesToCook": []
-  },
-  {
-    "id": 38,
-    "name": "Adah Ondricka",
-    "recipesToCook": []
-  },
-  {
-    "id": 39,
-    "name": "Estel Gutmann",
-    "recipesToCook": []
-  },
-  {
-    "id": 40,
-    "name": "Janelle Ullrich",
-    "recipesToCook": []
-  },
-  {
-    "id": 41,
-    "name": "Izaiah Stroman",
-    "recipesToCook": []
-  },
-  {
-    "id": 42,
-    "name": "Breana Hartmann",
-    "recipesToCook": []
-  },
-  {
-    "id": 43,
-    "name": "Greta Legros",
-    "recipesToCook": []
-  },
-  {
-    "id": 44,
-    "name": "Alan Grimes",
-    "recipesToCook": []
-  },
-  {
-    "id": 45,
-    "name": "Jameson Reichel",
-    "recipesToCook": []
-  },
-  {
-    "id": 46,
-    "name": "Adrain Donnelly",
-    "recipesToCook": []
-  },
-  {
-    "id": 47,
-    "name": "Jackie Robel",
-    "recipesToCook": []
-  },
-  {
-    "id": 48,
-    "name": "Wanda Stanton",
-    "recipesToCook": []
-  },
-  {
-    "id": 49,
-    "name": "Clint Rodriguez",
-    "recipesToCook": []
-  }
-]
+const usersData = [
+    {
+      "name": "Saige O'Kon",
+      "id": 1,
+      "recipesToCook": []
+    },
+    {
+      "name": "Ephraim Goyette",
+      "id": 2,
+      "recipesToCook": []
+    },
+    {
+      "name": "Nelda Bosco",
+      "id": 3,
+      "recipesToCook": []
+    },
+    {
+      "name": "Clinton Goodwin",
+      "id": 4,
+      "recipesToCook": []
+    },
+    {
+      "name": "Buford DuBuque",
+      "id": 5,
+      "recipesToCook": []
+    },
+    {
+      "name": "Myrl Crooks",
+      "id": 6,
+      "recipesToCook": []
+    },
+    {
+      "name": "Alberto White",
+      "id": 7,
+      "recipesToCook": []
+    },
+    {
+      "name": "Bennett Schroeder",
+      "id": 8,
+      "recipesToCook": []
+    },
+    {
+      "name": "Cullen Nicolas",
+      "id": 9,
+      "recipesToCook": []
+    },
+    {
+      "name": "Retta Douglas",
+      "id": 10,
+      "recipesToCook": []
+    },
+    {
+      "name": "Jarrod Senger",
+      "id": 11,
+      "recipesToCook": []
+    },
+    {
+      "name": "Caitlyn Schroeder",
+      "id": 12,
+      "recipesToCook": []
+    },
+    {
+      "name": "Valerie Grant",
+      "id": 13,
+      "recipesToCook": []
+    },
+    {
+      "name": "Stanford Doyle",
+      "id": 14,
+      "recipesToCook": []
+    },
+    {
+      "name": "Santina Deckow",
+      "id": 15,
+      "recipesToCook": []
+    },
+    {
+      "name": "Trevor Robel",
+      "id": 16,
+      "recipesToCook": []
+    },
+    {
+      "name": "Hardy Sipes",
+      "id": 17,
+      "recipesToCook": []
+    },
+    {
+      "name": "Missouri Runolfsdottir",
+      "id": 18,
+      "recipesToCook": []
+    },
+    {
+      "name": "Dusty Hauck",
+      "id": 19,
+      "recipesToCook": []
+    },
+    {
+      "name": "Jaden Kuhlman",
+      "id": 20,
+      "recipesToCook": []
+    },
+    {
+      "name": "Franz Batz",
+      "id": 21,
+      "recipesToCook": []
+    },
+    {
+      "name": "Alayna Davis",
+      "id": 22,
+      "recipesToCook": []
+    },
+    {
+      "name": "Earline Von",
+      "id": 23,
+      "recipesToCook": []
+    },
+    {
+      "name": "Blanche Price",
+      "id": 24,
+      "recipesToCook": []
+    },
+    {
+      "name": "Charlene Kuvalis",
+      "id": 25,
+      "recipesToCook": []
+    },
+    {
+      "name": "Yazmin Lubowitz",
+      "id": 26,
+      "recipesToCook": []
+    },
+    {
+      "name": "Domenick Russel",
+      "id": 27,
+      "recipesToCook": []
+    },
+    {
+      "name": "Arvid Greenholt",
+      "id": 28,
+      "recipesToCook": []
+    },
+    {
+      "name": "Peyton Little",
+      "id": 29,
+      "recipesToCook": []
+    },
+    {
+      "name": "Rosetta Roob",
+      "id": 30,
+      "recipesToCook": []
+    },
+    {
+      "name": "Dandre Gibson",
+      "id": 31,
+      "recipesToCook": []
+    },
+    {
+      "name": "Meaghan Padberg",
+      "id": 32,
+      "recipesToCook": []
+    },
+    {
+      "name": "Dawn Paucek",
+      "id": 33,
+      "recipesToCook": []
+    },
+    {
+      "name": "Moshe O'Reilly",
+      "id": 34,
+      "recipesToCook": []
+    },
+    {
+      "name": "Gladys Rohan",
+      "id": 35,
+      "recipesToCook": []
+    },
+    {
+      "name": "Bryon Baumbach",
+      "id": 36,
+      "recipesToCook": []
+    },
+    {
+      "name": "Lamar Brown",
+      "id": 37,
+      "recipesToCook": []
+    },
+    {
+      "name": "Christophe Deckow",
+      "id": 38,
+      "recipesToCook": []
+    },
+    {
+      "name": "Noe Conroy",
+      "id": 39,
+      "recipesToCook": []
+    },
+    {
+      "name": "Demarcus Gusikowski",
+      "id": 40,
+      "recipesToCook": []
+    },
+    {
+      "name": "Taya Bartell",
+      "id": 41,
+      "recipesToCook": []
+    },
+    {
+      "name": "Edward Ankunding",
+      "id": 42,
+      "recipesToCook": []
+    }
+  ]
 
-module.exports = userData;
+export default usersData
