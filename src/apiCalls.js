20626fea4ef78e83ac912fd09299b44f168f2b1d
ROlearyPro <rolearyprogramming@gmail.com>
2024-07-23 18:04:32 -0600

    Co-authored-by: Robert Phillips <robert-phillips33@users.noreply.github.com> Co-authored-by: Zelalem Fitru <ZTFitru@users.noreply.github.com> Co-authored-by: Charles T. Banks, Jr. <DRIF7ER@users.noreply.github.com> Co-authored-by: Willem Pol <wavpool@users.noreply.github.com>

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,9 @@
 // Your fetch requests will live here!
 let currentUser;
-const userPromise = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users").then((response) => response.json());
+const userPromise = fetch("http://localhost:3001/api/v1/users").then((response) => response.json());
 console.log(userPromise);
-const ingredientsPromise = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients").then((response) => response.json());
-const recipesPromise = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes").then((response) => response.json());
+const ingredientsPromise = fetch("http://localhost:3001/api/v1/ingredients").then((response) => response.json());
+const recipesPromise = fetch("http://localhost:3001/api/v1/recipes").then((response) => response.json());
 
 let userList;
 let ingredientList;
